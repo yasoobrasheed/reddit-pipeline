7b5da7495cbca0a44a4b6728e794ee2db5a2828d
Yasoob Rasheed <yasoob.f.rasheed@gmail.com>
2022-09-05 16:17:53 -0500

    set up SAM template and clean up repo

--- app/lambdas/subreddit_stream.py
+++ app/lambdas/subreddit_stream.py
@@ -1,8 +1,8 @@
 import os
-from venv import create
-from dotenv import load_dotenv
-from datetime import datetime, timedelta
+import json
 import praw
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
 
 def access_reddit():
@@ -18,10 +18,11 @@ def parse_subreddits(reddit, subreddits):
     subreddit = reddit.subreddit(subreddits)
     last_five_minutes_utc = datetime.utcnow() - timedelta(minutes=5)
 
-    for submission in subreddit.new(limit=25):
+    for submission in subreddit.new(limit=100):
         created_utc = datetime.utcfromtimestamp(submission.created_utc)
         was_created_in_last_five_minutes = last_five_minutes_utc <= created_utc
 
+        # TODO: Write each event to SQS
         if was_created_in_last_five_minutes:
             print("----------------------")
             print("subreddit: " + str(submission.subreddit))
@@ -32,12 +33,19 @@ def parse_subreddits(reddit, subreddits):
             return
 
 
-def main():
+def lambda_handler(event, context):
     load_dotenv()
-    reddit = access_reddit()  # set a 23 hour interval here in parallel
-    subreddits = "news+worldnews+politics"
-    parse_subreddits(reddit, subreddits)
+    reddit = access_reddit()
+    SUBREDDITS = "news+worldnews+politics"
+    parse_subreddits(reddit, SUBREDDITS)
 
-
-if __name__ == "__main__":
-    main()
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "Successfully streamed all new reddit posts from "
+                + SUBREDDITS
+                + " in the past 5 minutes."
+            }
+        ),
+    }
